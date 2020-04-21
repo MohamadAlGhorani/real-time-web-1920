d7743d68488d6e6284accf01380ad2539359ea2e
MohamadAlGhorani <moh.gh266@gmail.com>
2020-04-21 22:21:53 +0200

    userName added

--- server.js
+++ server.js
@@ -115,7 +115,6 @@ app.get("/setup", function (req, res) {
     }
   }).then(async response => {
     const data = await response.json();
-    userName = data.display_name
     fetch(`https://api.spotify.com/v1/users/${data.id}/playlists`, {
       headers: {
         'Content-Type': 'application/json',
@@ -135,14 +134,21 @@ app.get("/setup", function (req, res) {
 app.get("/party-:id", function (req, res) {
   const token = req.cookies.accessToken;
   console.log(token);
-  fetch(`https://api.spotify.com/v1/playlists/${req.params.id}/tracks`, {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    }
-  }).then(async response => {
-    const tracksData = await response.json();
-    console.log(tracksData)
+  Promise.all([
+    fetch(`https://api.spotify.com/v1/playlists/${req.params.id}/tracks`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      }
+    }).then(response => response.json()),
+    fetch("https://api.spotify.com/v1/me", {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      }
+    }).then(response => response.json())
+  ]).then(([tracksData, data]) => {
+    userName = data.display_name
     res.render("party", {
       title: "Party",
       tracksData: tracksData,
