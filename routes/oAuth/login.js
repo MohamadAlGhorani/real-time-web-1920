7f3da884964be61edf6d9709a49aff23854c10b7
Mohamad Al Ghorani <45425087+MohamadAlGhorani@users.noreply.github.com>
2024-09-02 12:02:46 +0200

    try new way of response handeling

--- routes/oAuth/login.js
+++ routes/oAuth/login.js
@@ -1,11 +1,5 @@
 require("dotenv").config();
 
-const {
-    SPOTIFY_REDIRECT_URI,
-    SPOTIFY_CLIENT_SECRET,
-    SPOTIFY_CLIENT_ID,
-} = process.env;
-
 module.exports = function (req, res) {
     var scopes = 'streaming user-read-private user-read-email user-read-playback-state user-modify-playback-state user-top-read';
     const {
--- routes/setup.js
+++ routes/setup.js
@@ -6,14 +6,23 @@ module.exports = function (req, res) {
         headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${token}`,
+            'Accept-Encoding': 'gzip, deflate, br' // Ensure the server knows we accept compressed responses
         }
     }).then(async response => {
-        console.log('setup', response);
-        const data = await response.json();
+        let data;
+        try {
+            const text = await response.text(); // Get the response as text
+            console.log('Response body:', text); // Log the response body
+            data = JSON.parse(text); // Try to parse the text as JSON
+        } catch (error) {
+            console.error(`Invalid JSON response body at ${response.url} reason: ${error.message}`);
+            throw new Error(`Invalid JSON response: ${error.message}`);
+        }
         fetch(`https://api.spotify.com/v1/users/${data.id}/playlists`, {
             headers: {
                 'Content-Type': 'application/json',
                 Authorization: `Bearer ${token}`,
+                'Accept-Encoding': 'gzip, deflate, br' // Ensure the server knows we accept compressed responses
             }
         }).then(async response => {
             const playlistsData = await response.json();
